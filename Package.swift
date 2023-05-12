9f79a09df05e4370ed8a86e8a9e57ac8d34d0b49
Jonathan Wight <jwight@mac.com>
2023-05-11 17:48:11 -0700

    WIP

--- Package.swift
+++ Package.swift
@@ -6,14 +6,14 @@ import PackageDescription
 let package = Package(
     name: "SwiftFields",
     products: [
-        // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
             name: "SwiftFields",
             targets: ["SwiftFields"]),
     ],
+    dependencies: [
+        .package(url: "https://github.com/schwa/SwiftFormats", branch: "main")
+    ],
     targets: [
-        // Targets are the basic building blocks of a package, defining a module or a test suite.
-        // Targets can depend on other targets in this package and products from dependencies.
         .target(
             name: "SwiftFields"),
         .testTarget(
