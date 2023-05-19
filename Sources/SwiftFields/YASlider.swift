fde72a6168cba6ae5c073f5ccb06a61858da7cd3
Jonathan Wight <jwight@mac.com>
2023-05-19 10:15:19 -0700

    Incorporate LineSegments to clean up YASlider drawing code considerably.

--- Sources/SwiftFields/YASlider.swift
+++ Sources/SwiftFields/YASlider.swift
@@ -18,42 +18,58 @@ public struct YASlider: View {
 
     public var body: some View {
         let geometry = controlSize.pathSliderGeometry
-        let halfThumbSize = CGSize(width: geometry.thumbSize.width * 0.5, height: geometry.thumbSize.height * 0.5)
         switch axis {
         case .horizontal:
             GeometryReader { proxy in
-                let trackPath = Path.line(
-                    from: CGPoint(x: geometry.trackWidth / 2, y: halfThumbSize.height),
-                    to: CGPoint(x: proxy.size.width - geometry.trackWidth / 2, y: halfThumbSize.height)
-                )
-                let thumbPath = Path.line(
-                    from: CGPoint(x: halfThumbSize.width, y: halfThumbSize.height),
-                    to: CGPoint(x: proxy.size.width - halfThumbSize.width, y: halfThumbSize.height)
-                )
+                let (trackPath, thumbPath) = paths(for: proxy.size)
                 PathSlider(value: _value, in: limit, trackPath: trackPath, thumbPath: thumbPath)
             }
             .frame(height: geometry.thumbSize.height)
             .frame(minWidth: geometry.thumbSize.width)
         case .vertical:
             GeometryReader { proxy in
-                let trackPath = Path.line(
-                    from: CGPoint(x: halfThumbSize.width, y: proxy.size.height - geometry.trackWidth / 2),
-                    to: CGPoint(x: halfThumbSize.width, y: geometry.trackWidth / 2)
-                )
-                let thumbPath = Path.line(
-                    from: CGPoint(x: halfThumbSize.width, y: proxy.size.height - halfThumbSize.height),
-                    to: CGPoint(x: halfThumbSize.width, y: halfThumbSize.height)
-                )
+                let (trackPath, thumbPath) = paths(for: proxy.size)
                 PathSlider(value: _value, in: limit, trackPath: trackPath, thumbPath: thumbPath)
             }
             .frame(width: geometry.thumbSize.width)
             .frame(minHeight: geometry.thumbSize.height)
         }
     }
+
+    private func paths(for size: CGSize) -> (trackPath: Path, thumbPath: Path) {
+        let geometry = controlSize.pathSliderGeometry
+        let halfThumbSize = CGSize(width: geometry.thumbSize.width * 0.5, height: geometry.thumbSize.height * 0.5)
+        switch axis {
+        case .horizontal:
+            let length = size.width
+            let trackPath = LineSegment(y: 0, from: 0, to: length)
+                .insetBy(dx: geometry.trackWidth / 2)
+                .offsetBy(dy: halfThumbSize.height)
+            let thumbPath = LineSegment(y: 0, from: 0, to: length)
+                .insetBy(dx: halfThumbSize.width)
+                .offsetBy(dy: halfThumbSize.height)
+            return (trackPath: Path(trackPath), thumbPath: Path(thumbPath))
+
+        case .vertical:
+            let length = size.height
+            let trackPath = LineSegment(x: 0, from: length, to: 0)
+                .insetBy(dy: geometry.trackWidth / 2)
+                .offsetBy(dx: halfThumbSize.height)
+            let thumbPath = LineSegment(x: 0, from: length, to: 0)
+                .insetBy(dy: halfThumbSize.width)
+                .offsetBy(dx: halfThumbSize.height)
+            return (trackPath: Path(trackPath), thumbPath: Path(thumbPath))
+        }
+    }
 }
+
+// MARK: -
 
 struct YASlider_Preview: PreviewProvider {
     static var previews: some View {
-        YASlider(value: .constant(0.5), in: 0 ... 1, axis: .horizontal)
+        VStack {
+            YASlider(value: .constant(0.5), axis: .horizontal)
+            YASlider(value: .constant(0.5), axis: .vertical).frame(height: 50)
+        }
     }
 }
